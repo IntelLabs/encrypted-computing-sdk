22c660365ff72cfe8ca450298544a1672878f442
Rojas Chaves, Jose <jose.rojas.chaves@intel.com>
2025-06-13 21:57:07 +0000

    Fixing license year

--- assembler_tools/hec-assembler-tools/assembler/instructions/cinst/cinstruction.py
+++ assembler_tools/hec-assembler-tools/assembler/instructions/cinst/cinstruction.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024 Intel Corporation
+# Copyright (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 """CInstruction base class for C-instructions in the assembler."""
