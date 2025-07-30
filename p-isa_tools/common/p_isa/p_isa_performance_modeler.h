da57e46ca9db917b94e0708a65b906c87d718af1
Alexander Viand <alexander.viand@intel.com>
2025-07-30 11:34:22 -0700

    remove duplicate license headers

--- p-isa_tools/common/p_isa/p_isa_performance_modeler.h
+++ p-isa_tools/common/p_isa/p_isa_performance_modeler.h
@@ -1,9 +1,6 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-// Copyright (C) 2024 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-
 #pragma once
 
 #include <chrono>
